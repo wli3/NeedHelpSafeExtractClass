ec2f8cacc0aa6ce0c6bcb5a973d6dbefe5219cbb
William Li <wul@microsoft.com>
2018-03-03 09:46:14 -0800

    Move to another type, fix access level and fix namespace

---
+++ FeedChecker/Feed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FeedChecker
+{
+    public static class Feed
+    {
+        public static IEnumerable<string> GetAllCoreFxPreview1(string feed)
+        {
+            if (!feed.EndsWith("/"))
+                feed = feed + "/";
+
+            var packageUrl = feed + "main/binary-amd64/Packages";
+            var request = (HttpWebRequest) WebRequest.Create(packageUrl);
+
+            var result = "";
+            var response = request.GetResponse();
+            using (var responseStream = response.GetResponseStream())
+            {
+                var reader = new StreamReader(responseStream, Encoding.UTF8);
+                result = reader.ReadToEnd();
+            }
+            var lines = result.Split(
+                new[] {"\r\n", "\r", "\n"},
+                StringSplitOptions.None);
+
+            var onlyCoreFxPreview1ButWithPackgeInFront =
+                lines.Where(l => l.StartsWith("Package: dotnet-hostfxr-2.0.0-preview1"));
+
+            // imagine there is more code
+
+            return onlyCoreFxPreview1ButWithPackgeInFront.Select(l => l.Replace("Package: ", ""));
+        }
+    }
+}
--- FeedCheckerTest/FeedCheckerTest.cs
+++ FeedCheckerTest/FeedCheckerTest.cs
@@ -9,14 +9,14 @@ namespace FeedCheckerTest
         [Fact]
         public void WhenFeedUrlIsProvidedItListAllCorefxpreview1Version()
         {
-            var result = Program.GetAllCoreFxPreview1("http://apt-mo.trafficmanager.net/repos/dotnet/dists/jessie");
+            var result = Feed.GetAllCoreFxPreview1("http://apt-mo.trafficmanager.net/repos/dotnet/dists/jessie");
             result.Should().HaveCountGreaterOrEqualTo(115);
         }
 
         [Fact]
         public void WhenFeedUrlIsProvidedItListCorefxpreview1VersionWithFollowingFormat()
         {
-            var result = Program.GetAllCoreFxPreview1("http://apt-mo.trafficmanager.net/repos/dotnet/dists/jessie");
+            var result = Feed.GetAllCoreFxPreview1("http://apt-mo.trafficmanager.net/repos/dotnet/dists/jessie");
             foreach (var l in result)
                 l.Should().StartWith("dotnet-hostfxr-2.0.0-preview1");
         }
